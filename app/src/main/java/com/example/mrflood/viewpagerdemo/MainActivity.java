b1f4e52befc47a00118f06cecc7af58f766d4309
hongwei <1833005983@qq.com>
2018-03-12 09:28:09 +0800

    超简单模式实现小白点导航

--- app/src/main/java/com/example/mrflood/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/example/mrflood/viewpagerdemo/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,49 +20,43 @@ public class MainActivity extends AppCompatActivity {
     ViewPager pager;
     List<View> list;
     String[] titles = {"一", "二", "三", "四", "五", "六"};
-    LinearLayout continer;
+    RadioGroup continer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypager);
+
         pager = findViewById(R.id.mypager);
         continer = findViewById(R.id.continer);
+
         LayoutInflater inflater = LayoutInflater.from(this);
+
         list = new ArrayList<>();
+
         list.add(inflater.inflate(R.layout.layout1, null));
         list.add(inflater.inflate(R.layout.layout2, null));
         list.add(inflater.inflate(R.layout.layout3, null));
         list.add(inflater.inflate(R.layout.layout4, null));
         list.add(inflater.inflate(R.layout.layout5, null));
         list.add(inflater.inflate(R.layout.layout6, null));
+
         pager.setAdapter(new MyPager());
+
         pager.setPageTransformer(true, new DepthPageTransformer());
-        pager.setPageTransformer(true, new ZoomOutPageTransformer());
+
         pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
 
             }
 
             @Override
             public void onPageSelected(int position) {
-                for (int i = position - 1; i <= position + 1; i++) {
-                    Log.d("问", "onPageSelected: " + i);
-                    LinearLayout view = (LinearLayout) continer.getChildAt(i);
-                    if (view == null) continue;
-                    if (i == position) {
-                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(50, 50);
-                        params.rightMargin = 35;
-                        view.setLayoutParams(params);
-                        view.setBackgroundResource(R.drawable.white);
-                    } else {
-                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(35, 35);
-                        params.rightMargin = 35;
-                        view.setLayoutParams(params);
-                        view.setBackgroundResource(R.drawable.black);
-                    }
-                }
+                RadioButton button = (RadioButton) continer.getChildAt(position);
+                button.toggle();
             }
 
             @Override
@@ -110,56 +106,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class DepthPageTransformer implements ViewPager.PageTransformer {
-        private static final float MIN_SCALE = 0.75f;
 
         public void transformPage(View view, float position) {
-            Log.d("DepthPageTransformer", view.getTag() + " , " + position + "");
-            int pageWidth = view.getWidth();
-            if (position < -1) { // [-Infinity,-1)
-                // This page is way off-screen to the left.
-                view.setAlpha(0);
-            } else if (position <= 0) { // [-1,0]
-                // Use the default slide transition when moving to the left page
-                view.setAlpha(1);
-                view.setTranslationX(0);
-                view.setScaleX(1);
-                view.setScaleY(1);
-            } else if (position <= 1) { // (0,1]
-                // Fade the page out.
-                view.setAlpha(1 - position);
-                // Counteract the default slide transition
-                view.setTranslationX(pageWidth * -position);
-                // Scale the page down (between MIN_SCALE and 1)
-                float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
-                view.setScaleX(scaleFactor);
-                view.setScaleY(scaleFactor);
-            } else { // (1,+Infinity]
-                // This page is way off-screen to the right.
-                view.setAlpha(0);
+            int width = view.getWidth();
+            int pivotX = 0;
+            if (position <= 1 && position > 0) {// right scrolling
+                pivotX = 0;
+            } else if (position == 0) {
+
+            } else if (position < 0 && position >= -1) {// left scrolling
+                pivotX = width;
             }
+            //设置x轴的锚点
+            view.setPivotX(pivotX);
+            //设置绕Y轴旋转的角度
+            view.setRotationY(90f * position);
         }
     }
 
     public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
-        boolean isget = false;
-        int width;
-        int height;
 
-        public void transformPage(View view, float position) {
+        public void transformPage(View page, float position) {
             /*https://www.2cto.com/kf/201608/540650.html*/
-            Log.e("TAG", view + " , " + position + "");
-            if ((position < 0 && position > -1) || (position > 0 && position < 1)) {
-                if (!isget) {
-                    width = view.getWidth();
-                    height = view.getHeight();
-                    isget = true;
-                }
-            } else {
-                view.setX(width/2);
-                view.setY(height/2);
-                float scaleFactor = 1 - Math.abs(position);
-                view.setScaleX(scaleFactor);
-                view.setScaleY(scaleFactor);
+            Log.e("TAG", page + " , " + position + "");
+            int width = page.getWidth();
+            int height = page.getHeight();
+
+            //這裏只對右邊的view做了操作
+            if (position > 0 && position <= 1) {//right scorlling
+                //position是1.0->0,但是沒有等於0
+                float minScale = 0.8f;
+                position = minScale < position ? minScale : position;
+                Log.e("TAG", "right----position====" + position);
+                //設置該view的X軸不動
+//                page.setTranslationX(-width * position);
+                //設置縮放中心點在該view的正中心
+                page.setPivotX(width / 2);
+                page.setPivotY(height / 2);
+                //設置縮放比例（0.0，1.0]
+                page.setScaleX(1 - position);
+                page.setScaleY(1 - position);
+                page.setAlpha(1 - position);
+            } else if (position >= -1 && position < 0) {//left scrolling
+
+            } else {//center
+
             }
         }
     }
